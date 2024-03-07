9890311a30fad8923c7295a86c68abcc560a7620
muhammadpadanta <dantagaul22@gmail.com>
2024-03-07 23:43:49 +0700

    Deleted _app.tsx

--- pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import 'src/app/global.css';
-import { AppProps } from 'next/app';
-
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-
-
