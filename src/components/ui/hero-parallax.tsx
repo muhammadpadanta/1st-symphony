45248929663d26aebd9b9b2ea59bec0ce3459885
muhammadpadanta <dantagaul22@gmail.com>
2024-03-25 15:28:09 +0700

    Revisi UI lagi

--- src/components/ui/hero-parallax.tsx
+++ src/components/ui/hero-parallax.tsx
@@ -105,7 +105,7 @@ export const Header = () => {
   return (
     <div className="max-w-7xl relative mx-auto py-20 md:py-40 px-4 w-full  left-0 top-0">
       <h1 className="text-2xl md:text-7xl font-bold dark:text-white new-rocker-regular">
-      <span className="underline">Symphony Source</span><br/>Your Gateway to Unforgettable Concerts
+      <span className="underline">Symphony Sources</span><br/>Your Gateway to Unforgettable Concerts
       </h1>
       <p className="max-w-2xl text-base md:text-xl mt-8 dark:text-neutral-200 new-rocker-regular">
       Welcome to <span className="bg-gradient-to-r from-amber-500 to-pink-500 bg-clip-text text-transparent">1st Symphony</span>, your go-to destination for purchasing music concert tickets online! 
