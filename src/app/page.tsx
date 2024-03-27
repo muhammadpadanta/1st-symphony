763ffdb7bcd0d8261b6047c702c6c7250bf9bafd
muhammadpadanta <dantagaul22@gmail.com>
2024-03-27 16:32:17 +0700

    Revision

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,20 +4,18 @@ import {HeroHome} from "@/components/herohome";
 import { HomeParallaxEffect } from "@/components/homeparallax";
 import Footer from '../components/footer';
 import {FaqMenu} from "@/components/faq";
-import {NextUIProvider} from "@nextui-org/react";
-import LoginModal from "@/components/loginmodal";
+
+
 export default function Home() {
   return (
-      <NextUIProvider>
+
     <main className="bg-[#0a0a0a]">
       <NavbarMenu />
       <HeroHome />
       <HomeParallaxEffect/>
       <FaqMenu />
       <Footer />
-      <LoginModal/>
 
     </main>
-          </NextUIProvider>
   );
 }
