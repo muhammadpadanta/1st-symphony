14d401ab65a42762f74232aed63f749c3563ef80
muhammadpadanta <dantagaul22@gmail.com>
2024-04-07 12:09:15 +0700

    Fixing Navbar

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -5,7 +5,7 @@ import "../../styles/globals.css"
 const Layout: React.FC<PropsWithChildren<{}>> = ({ children }) => {
     return (
         <div className="flex flex-col min-h-screen ">
-            <Navbar className="pacifico-regular sticky top-0 z-50 w-full overflow-x-hidden"/>
+            <Navbar className="pacifico-regular sticky top-0 z-50 w-full "/>
                 {children}
             <Footer className="pacifico-regular"/>
         </div>
